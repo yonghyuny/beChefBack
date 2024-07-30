c41342e7c630f4c0a3991188affe7b758ed4e4af
yonghyuny <lovelove4695@naver.com>
2024-07-30 20:51:15 +0900

    domain

--- src/main/java/com/example/bechef/security/WebConfig.java
+++ src/main/java/com/example/bechef/security/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")    // 모든 주소 적용됨
                 .allowedOrigins("Http://localhost:3000") // 테스트용 도메인
-//                .allowedOrigins("https://icy-water-02a4fb800.5.azurestaticapps.net") // 배포용 도메인
+                .allowedOrigins("https://icy-water-02a4fb800.5.azurestaticapps.net") // 배포용 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // HTTP request 메서드 허용
                 .allowedHeaders("*") // request의 모든 header 허용
                 .exposedHeaders("Authorization") // 클라이언트가 authorization은 볼수있게 해줌
